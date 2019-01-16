1c05902fac47f1d075f327c3d611c265221af8e0
anjalisg <anjalisgopinathan@gmail.com>
2019-01-16 10:17:18 -0800

    graph

--- Thermocouple_TFT_DRS/TestTFT_DRS_master/TestTFT_DRS_master.ino
+++ Thermocouple_TFT_DRS/TestTFT_DRS_master/TestTFT_DRS_master.ino
@@ -12,11 +12,12 @@
 #define BLACKCOLOR 0x0000  // Black
 #define BLUECOLOR 0x076eff //Blue
 #define GREENCOLOR 0x45ad47 //Green
+#define LIMEGREENCOLOR 0x00FF00
 
 Adafruit_RA8875 tft = Adafruit_RA8875(RA8875_CS, RA8875_RESET);
 Adafruit_MAX31856 tc1 = Adafruit_MAX31856(38, 39, 37, 36);
 
-uint16_t tx, ty;
+//uint16_t tx, ty;
 
 void setup() 
 {
@@ -35,40 +36,19 @@ void setup()
   tft.GPIOX(true);      // Enable TFT - display enable tied to GPIOX
   tft.PWM1config(true, RA8875_PWM_CLK_DIV1024); // PWM output for backlight
   tft.PWM1out(255);
-  tft.fillScreen(RA8875_BLACK);
-
+//  tft.fillScreen(RA8875_BLACK);
+  tft.fillScreen(BLACKCOLOR);
   tft.textMode();
   
   tft.textSetCursor(10, 10);
 
-  tft.setRotation(1);
+  //tft.setRotation(0);
   /* Render some text! */
 
-  //tft.drawLine(100, 50, 100, 380,0x000000); // draw a y-axis across the screen
-//  for(int x=100; x<110; x++){
-//    tft.drawFastVLine(x,50,230, WHITECOLOR);
-//  }
-//
-//  tft.stroke(255, 255, 255); // set the stroke color to white
-//  tft.line(98, 378, 780, 98); // draw a x-axis across the screen
-//  for(int y=50; y<=380; y++){
-//    tft.drawPixel(y,100,0x000000);
-//  }
+
 //longest temp time = 230 min
-
-  tft.textTransparent(RA8875_WHITE);
-
-  tft.textSetCursor(50, 250);
-  tft.textEnlarge(2);
-//  tft.textWrite("Current temp: ");
-}
-
-void loop() 
-{
-   tft.fillScreen(RA8875_BLACK);
-
 //Draw Graph outline
-  tft.drawRect(100, 50, 690, 330, WHITECOLOR);    //drawRect(x0, y0, width, height, color)
+  tft.drawRect(60, 50, 730, 350, WHITECOLOR);    //drawRect(x0, y0, width, height, color)
 
   tft.textTransparent(RA8875_WHITE);
   tft.textSetCursor(635, 440);
@@ -77,26 +57,42 @@ void loop()
   tft.textSetCursor(0,0);
   tft.textWrite("Temp (C)        Temperature vs time");
   
-  tft.textSetCursor(50,55);
-  tft.textWrite("90");
-  //tft.textSetCursor(100,390);
-  //tft.textWrite("0");
+//  tft.textSetCursor(10,55);
+//  tft.textWrite("90");
 
+  //x axis labels:
   char minuteNumber_String[3];
   tft.textEnlarge(0.6);
-
-  for(int x=80; x<=790; x+=60){
-    tft.textSetCursor(x,390);
-    dtostrf((x-80)/3, 3, 0, minuteNumber_String);
+  
+  for(int x=50; x<=790; x+=60){
+    tft.textSetCursor(x,410);
+    dtostrf(  ( (x-50)/3 )  , 3, 0, minuteNumber_String);
 
     tft.textWrite(minuteNumber_String);
   }
+
+  //y axis labels:
+  char tempValue_String[1];
+  for(int y=390; y>=40; y-=20){
+    tft.textSetCursor(10,y);
+    dtostrf( (int)(( 390-y )/4  ), 2, 0, tempValue_String);
+    tft.textWrite(tempValue_String);
+  }
+  //start/stop button:
 //  tft.fillRect(0,275,80,50,BLUECOLOR);  //blue
 //  
 //  tft.textSetCursor(0,280);
 //  tft.textEnlarge(0.7);
 //  tft.textTransparent(GREENCOLOR);
 //  tft.textWrite("Start");
+    tft.drawPixel(780,10,LIMEGREENCOLOR);
+
+}
+
+void loop() 
+{
+   //tft.fillScreen(RA8875_BLACK);
+  tft.fillRect(100,440,480,40,BLACKCOLOR);  //refresh the current temp label
 
   float currentTemp = tc1.readThermocoupleTemperature();  //degrees celsius
   char temperature_string[5];
@@ -106,11 +102,19 @@ void loop()
   Serial.print("Current temp: ");
   Serial.println(currentTemp);
 
+  tft.textEnlarge(1);
   tft.textTransparent(RA8875_WHITE);
   tft.textSetCursor(100,440);
   tft.textWrite("Current temp (C): ");
   tft.textWrite(temperature_string);
 
-  delay(700);
+
+//  int TwentySecondPixel = 60;
+//  int TempPixel = 400- ((int)(currentTemp) *4);
+////  tft.drawLine(TwentySecondPixel,); //drawLine(x0,y0,x1,y1,color);
+//  tft.drawPixel(TwentySecondPixel, TempPixel, LIMEGREENCOLOR);
+//  TwentySecondPixel++;
+
+  delay(1000);//20 seconds refresh
 
 }
